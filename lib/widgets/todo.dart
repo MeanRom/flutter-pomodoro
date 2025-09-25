b43ae70492adcd550baaba607810693f49812abd
Thomas <moer.thomas10@gmail.com>
2025-09-25 19:45:11 +0200

    feat: focus input after submit

--- lib/widgets/todo.dart
+++ lib/widgets/todo.dart
@@ -11,6 +11,7 @@ class Todo extends StatefulWidget {
 
 class _TodoState extends State<Todo> {
   final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -22,10 +23,12 @@ class _TodoState extends State<Todo> {
             CupertinoTextField.borderless(
               placeholder: 'Add a new task',
               controller: _controller,
+              focusNode: _focusNode,
               onSubmitted: (value) {
                 if (value.trim().isNotEmpty) {
                   provider.addTodo(value.trim());
                   _controller.clear();
+                  FocusScope.of(context).requestFocus(_focusNode);
                 }
               },
               padding: const EdgeInsets.symmetric(vertical: 12),
